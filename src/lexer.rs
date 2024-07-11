b682c042f4f239a5abf5b56fe9b39edbab62e84a
Adib Fahimi <adibfahi@gmail.com>
2024-07-11 12:49:19 +0330

    refactor: update lexer and Expr structure
    - test: add new tests to lexer
    - fix: resolve small bug in lexer
    - refactor: improve Expr structure

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,44 +1,60 @@
-use std::fmt;
+#[derive(thiserror::Error, Debug)]
+pub enum LexerError {
+    #[error("Unexpected character: {0}")]
+    UnexpectedCharacter(char),
+
+    #[error("Unexpected end of input")]
+    UnexpectedEndOfInput,
+}
 
 #[derive(Debug, Clone, PartialEq)]
 pub enum Token {
-    Paren(char),
-    Operator(char),
-    Comma,
-    SemiColon,
-    ComparisonOperator(String),
-    Keyword(String),
     Identifier(String),
     Num(i64),
     Str(String),
     Bool(bool),
-}
 
-impl fmt::Display for Token {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            Token::Paren(c) => write!(f, "Paren({})", c),
-            Token::Operator(c) => write!(f, "Operator({})", c),
-            Token::Comma => write!(f, "Comma"),
-            Token::SemiColon => write!(f, "SemiColon"),
-            Token::ComparisonOperator(s) => write!(f, "ComparisonOperator({})", s),
-            Token::Keyword(s) => write!(f, "Keyword({})", s),
-            Token::Identifier(s) => write!(f, "Identifier({})", s),
-            Token::Num(n) => write!(f, "Num({})", n),
-            Token::Str(s) => write!(f, "Str({})", s),
-            Token::Bool(b) => write!(f, "Bool({})", b),
-        }
-    }
+    Paren(char),
+    Operator(char),
+    ComparisonOperator(String),
+    Keyword(String),
+
+    Comma,
+    SemiColon,
+    Colon,
+    Dot,
 }
 
 use Token::*;
 
-pub fn tokenize(source: &str) -> Vec<Token> {
+impl std::fmt::Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            Identifier(s) => write!(f, "Identifier({})", s),
+            Num(n) => write!(f, "Num({})", n),
+            Str(s) => write!(f, "Str({})", s),
+            Bool(b) => write!(f, "Bool({})", b),
+            Paren(c) => write!(f, "Paren({})", c),
+            Operator(c) => write!(f, "Operator({})", c),
+            ComparisonOperator(s) => write!(f, "ComparisonOperator({})", s),
+            Keyword(s) => write!(f, "Keyword({})", s),
+            Comma => write!(f, "Comma"),
+            SemiColon => write!(f, "SemiColon"),
+            Colon => write!(f, "Colom"),
+            Dot => write!(f, "Dot"),
+        }
+    }
+}
+
+pub fn tokenize(source: &str) -> Result<Vec<Token>, LexerError> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut pos = 0;
 
     while pos < source.len() {
-        let ch = source.chars().nth(pos).unwrap();
+        let ch = source
+            .chars()
+            .nth(pos)
+            .ok_or(LexerError::UnexpectedEndOfInput)?;
 
         match ch {
             '/' => {
@@ -50,6 +66,9 @@ pub fn tokenize(source: &str) -> Vec<Token> {
                     tokens.push(Operator('/'));
                     pos += 1;
                 }
+            }
+            '\n' | '\t' | ' ' => {
+                pos += 1;
             }
             '(' | ')' | '{' | '}' | '[' | ']' => {
                 tokens.push(Paren(ch));
@@ -85,11 +104,22 @@ pub fn tokenize(source: &str) -> Vec<Token> {
                     tokens.push(Operator('!'));
                 }
             }
+
+            '<' | '>' => {
+                if pos + 1 < source.len() && source.chars().nth(pos + 1).unwrap() == '=' {
+                    tokens.push(ComparisonOperator(format!("{}=", ch)));
+                    pos += 2;
+                } else {
+                    tokens.push(ComparisonOperator(ch.to_string()));
+                    pos += 1;
+                }
+            }
+
             ',' => {
                 tokens.push(Comma);
                 pos += 1;
             }
-            '+' | '-' | '*' | '%' | '^' | '<' | '>' => {
+            '+' | '-' | '*' | '%' | '^' => {
                 tokens.push(Operator(ch));
                 pos += 1;
             }
@@ -120,18 +150,157 @@ pub fn tokenize(source: &str) -> Vec<Token> {
                     _ => tokens.push(Identifier(identifier.to_string())),
                 }
             }
-            '\n' | '\t' | ' ' => {
+            ':' => {
+                tokens.push(Colon);
+                pos += 1;
+            }
+            '.' => {
+                tokens.push(Dot);
                 pos += 1;
             }
             _ => {
-                println!("Unexpected character: {} in {}", ch, pos);
+                return Err(LexerError::UnexpectedCharacter(ch));
             }
         }
     }
 
-    tokens
+    Ok(tokens)
 }
 
 fn is_valid_identifier_char(ch: char) -> bool {
     ch.is_alphanumeric() || ch == '_'
+}
+
+#[test]
+fn test_tokenize() {
+    let source = r#"
+    // sum of two numbers
+    function sum(a, b) {
+        return a + b;
+    }
+
+    var a = 1;
+    var b = 2;
+
+    var c = sum(a, b);
+
+    for (var i = 0; i < 10; i = i + 1) {
+        print(i);
+    }
+
+    while (a < 10) {
+        print(a);
+        a = a + 1;
+    }
+
+    if (a > 10) {
+        print("a is greater than 10");
+    } else {
+        print("a is less than 10");
+    }
+    "#;
+
+    let tokens = tokenize(source).unwrap();
+    let expected = vec![
+        Keyword("function".to_string()),
+        Identifier("sum".to_string()),
+        Paren('('),
+        Identifier("a".to_string()),
+        Comma,
+        Identifier("b".to_string()),
+        Paren(')'),
+        Paren('{'),
+        Keyword("return".to_string()),
+        Identifier("a".to_string()),
+        Operator('+'),
+        Identifier("b".to_string()),
+        SemiColon,
+        Paren('}'),
+        Keyword("var".to_string()),
+        Identifier("a".to_string()),
+        Operator('='),
+        Num(1),
+        SemiColon,
+        Keyword("var".to_string()),
+        Identifier("b".to_string()),
+        Operator('='),
+        Num(2),
+        SemiColon,
+        Keyword("var".to_string()),
+        Identifier("c".to_string()),
+        Operator('='),
+        Identifier("sum".to_string()),
+        Paren('('),
+        Identifier("a".to_string()),
+        Comma,
+        Identifier("b".to_string()),
+        Paren(')'),
+        SemiColon,
+        Keyword("for".to_string()),
+        Paren('('),
+        Keyword("var".to_string()),
+        Identifier("i".to_string()),
+        Operator('='),
+        Num(0),
+        SemiColon,
+        Identifier("i".to_string()),
+        ComparisonOperator("<".to_string()),
+        Num(10),
+        SemiColon,
+        Identifier("i".to_string()),
+        Operator('='),
+        Identifier("i".to_string()),
+        Operator('+'),
+        Num(1),
+        Paren(')'),
+        Paren('{'),
+        Identifier("print".to_string()),
+        Paren('('),
+        Identifier("i".to_string()),
+        Paren(')'),
+        SemiColon,
+        Paren('}'),
+        Keyword("while".to_string()),
+        Paren('('),
+        Identifier("a".to_string()),
+        ComparisonOperator("<".to_string()),
+        Num(10),
+        Paren(')'),
+        Paren('{'),
+        Identifier("print".to_string()),
+        Paren('('),
+        Identifier("a".to_string()),
+        Paren(')'),
+        SemiColon,
+        Identifier("a".to_string()),
+        Operator('='),
+        Identifier("a".to_string()),
+        Operator('+'),
+        Num(1),
+        SemiColon,
+        Paren('}'),
+        Keyword("if".to_string()),
+        Paren('('),
+        Identifier("a".to_string()),
+        ComparisonOperator(">".to_string()),
+        Num(10),
+        Paren(')'),
+        Paren('{'),
+        Identifier("print".to_string()),
+        Paren('('),
+        Str("a is greater than 10".to_string()),
+        Paren(')'),
+        SemiColon,
+        Paren('}'),
+        Keyword("else".to_string()),
+        Paren('{'),
+        Identifier("print".to_string()),
+        Paren('('),
+        Str("a is less than 10".to_string()),
+        Paren(')'),
+        SemiColon,
+        Paren('}'),
+    ];
+
+    assert_eq!(tokens, expected);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,14 @@ fn main() {
     let file_name = std::env::args().nth(1).expect("no file name given");
     let source = std::fs::read_to_string(file_name).expect("cannot read file");
 
-    let tokens = lexer::tokenize(source.as_str());
+    let tokens = match lexer::tokenize(source.as_str()) {
+        Ok(tokens) => tokens,
+        Err(e) => {
+            eprintln!("Error: {:?}", e);
+            std::process::exit(1);
+        }
+    };
+
     let asts = parser::parse(&tokens);
     let mut env = eval::Environment::new();
 
